7ec41895491e4b18d0eac5b0d5e91ee873530e06
asc-it-minds <asc@it-minds.dk>
2022-10-07 10:29:49 +0200

    Random grid with different types

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,102 +1,74 @@
-import { Button, TextField } from "@mui/material";
 import React, { useCallback, useEffect, useState } from "react";
 import "./Grid.css";
 
-type Settings = {
-    gridSize: number;
+type Props = {
+    grid?: Number[];
     height: number;
     width: number;
+    gridSize: number;
 };
 
-const Grid = () => {
-    const [rows, setRows] = useState([]);
-    const [height, setHeight] = useState(20);
-    const [width, setWidth] = useState(20);
-    const [gridSize, setgridSize] = useState(500);
-    const [settings, setSettings] = useState<Settings>();
+const typesOfFields = {
+    0: "grid-item",
+    1: "grid-item-fire",
+    2: "grid-item-forest",
+};
 
-    const onUpdate = () => {
-        if (settings.height !== settings.width) {
-            alert("Height and Width must be equal");
-            return;
-        }
-        setHeight(settings.height);
-        setWidth(settings.width);
-        setgridSize(settings.gridSize);
-    };
+const Grid = (props: Props) => {
+    const [grid, setGrid] = useState([]);
+
+    const drawGrid = useCallback(
+        (grid?: Number[]) => {
+            if (props.height !== props.grid.length) {
+                alert("mismatch");
+            }
+            try {
+                let initialRows = [];
+                let singleGridSize = props.gridSize / props.height;
+                let index = 0;
+                for (let i = 0; i < props.height; i++) {
+                    for (let j = 0; j < props.width; j++) {
+                        index++;
+                        initialRows.push(
+                            <div
+                                key={index}
+                                className={
+                                    grid
+                                        ? typesOfFields[grid[i][j]]
+                                        : "grid-item"
+                                }
+                                style={{
+                                    width: `${singleGridSize}px`,
+                                    height: `${singleGridSize}px`,
+                                }}
+                            />
+                        );
+                    }
+                }
+                setGrid(initialRows);
+            } catch (err) {
+                console.error("Error in grid provided");
+                console.error(err);
+            }
+        },
+        [props.grid.length, props.gridSize, props.height, props.width]
+    );
 
     const createGrid = useCallback(() => {
-        if (height !== width) {
-            alert("Height and Width must be equal");
-            return;
-        }
-        let initialRows = [];
-        let singleGridSize = gridSize / height;
-        for (let i = 0; i < height; i++) {
-            initialRows.push(
-                Array.from(Array(width), () => (
-                    <div
-                        key={Math.random()}
-                        id={`${i}`}
-                        className="grid-item"
-                        style={{
-                            width: `${singleGridSize}px`,
-                            height: `${singleGridSize}px`,
-                        }}
-                    />
-                ))
-            );
-        }
-        setRows(initialRows);
-    }, [gridSize, height, width]);
+        drawGrid(props.grid);
+    }, [drawGrid, props.grid]);
 
     useEffect(() => {
         createGrid();
-    }, [height, createGrid]);
+    }, [createGrid]);
 
     return (
         <div>
-            <div className="settings">
-                <TextField
-                    variant="outlined"
-                    label="Grid size in px"
-                    type="number"
-                    onChange={(e) =>
-                        setSettings({
-                            ...settings,
-                            gridSize: Number(e.target.value),
-                        })
-                    }
-                ></TextField>
-                <TextField
-                    variant="outlined"
-                    label="Height"
-                    type="number"
-                    onChange={(e) =>
-                        setSettings({
-                            ...settings,
-                            height: Number(e.target.value),
-                        })
-                    }
-                ></TextField>
-                <TextField
-                    variant="outlined"
-                    label="Width"
-                    type="number"
-                    onChange={(e) =>
-                        setSettings({
-                            ...settings,
-                            width: Number(e.target.value),
-                        })
-                    }
-                ></TextField>
-                <Button variant="contained" onClick={() => onUpdate()}>
-                    Update
-                </Button>
-            </div>
-            <br />
-            <div className="grid" style={{ height: gridSize, width: gridSize }}>
-                {rows}
+            <div
+                className="grid"
+                style={{ height: props.gridSize, width: props.gridSize }}
+            >
+                {grid}
             </div>
         </div>
     );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,79 @@
-import React from "react";
+import { Button, TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import Grid from "../components/Grid/Grid";
 
+type Settings = {
+    gridSize: number;
+    height: number;
+    width: number;
+};
+
 const MainPage = () => {
+    const [grid, setGrid] = useState<Number[]>();
+    const [height, setHeight] = useState(20);
+    const [width, setWidth] = useState(20);
+    const [gridSize, setgridSize] = useState(500);
+    const [settings, setSettings] = useState<Settings>();
+
+    const onClickUpdate = () => {
+        setGrid(null);
+        setHeight(settings.height);
+        setWidth(settings.width);
+        setgridSize(settings.gridSize);
+    };
+
+    useEffect(() => {
+        let grid = [];
+        for (let i = 0; i < height; i++) {
+            grid.push(Array(height));
+            for (let j = 0; j < height; j++) {
+                grid[i][j] = Math.round(Math.random() * 2);
+            }
+        }
+
+        setGrid(grid);
+    }, [height]);
+
     return (
         <div>
             <h1>Forest Fire Simulation</h1>
-            <Grid />
+            <div className="settings">
+                <TextField
+                    variant="outlined"
+                    label="Pixel size of grid"
+                    type="number"
+                    onChange={(e) =>
+                        setSettings({
+                            ...settings,
+                            gridSize: Number(e.target.value),
+                        })
+                    }
+                ></TextField>
+                <TextField
+                    variant="outlined"
+                    label="No of rows"
+                    type="number"
+                    onChange={(e) =>
+                        setSettings({
+                            ...settings,
+                            width: Number(e.target.value),
+                            height: Number(e.target.value),
+                        })
+                    }
+                ></TextField>
+                <Button variant="contained" onClick={() => onClickUpdate()}>
+                    Update
+                </Button>
+            </div>
+            <br />
+            {grid && (
+                <Grid
+                    grid={grid}
+                    gridSize={gridSize}
+                    width={width}
+                    height={height}
+                />
+            )}
         </div>
     );
 };
